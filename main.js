ffcf4af9ab67e9738b4aa932e9cb426b5b20fca6
Mateo <mateo.nicoloff25@gmail.com>
2023-10-28 08:26:07 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,15 @@
+/* Contenedores */
+let formulario = document.getElementById("contenedor__formulario");
+let gracias = document.getElementById("contenedor__gracias");
+
+/* Cambiar pagina */
+function mostrarGracias() {   
+    gracias.style.display="flex";
+    formulario.style.display="none";
+  
+}
+
+function mostrarFormulario() {
+  formulario.style.display = "flex";
+  gracias.style.display = "none";  
+}
